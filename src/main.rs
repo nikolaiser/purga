91c1bcb2d84c2db16237e8dd781ab5bb3588e5a9
nikolaiser <mail@nikolaiser.com>
2024-05-06 16:03:00 +0200

    Add support for arrays

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use serde::Serialize;
 use serde_json::json;
 use std::collections::HashMap;
 use std::error::Error;
@@ -11,6 +12,7 @@ struct Args {
     #[arg(short, long, default_value = "purgaArgs")]
     input: String,
     /// Key-value pairs to pass to the flake. Format is '--arg name1=value1 --arg name2=value2'
+    /// If you provide the same key multiple times it will be passed to the flake as an array
     #[arg(short, long = "arg", number_of_values = 1, value_parser = parse_key_val::<String, String>)]
     args: Vec<(String, String)>,
     /// Nix command to call
@@ -32,9 +34,43 @@ where
     Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
 }
 
+#[derive(Serialize, Debug)]
+#[serde(untagged)]
+enum PurgaValue {
+    Str(String),
+    StrVec(Vec<String>),
+}
+
+fn transform(input: Vec<(String, String)>) -> HashMap<String, PurgaValue> {
+    let mut map = HashMap::new();
+
+    for (key, value) in input {
+        match map.get_mut(&key) {
+            Some(PurgaValue::Str(existing)) => {
+                // If the key already exists and it's a single String, convert to a vector
+                let mut vec = Vec::new();
+                vec.push(existing.clone());
+                vec.push(value);
+                map.insert(key, PurgaValue::StrVec(vec));
+            }
+            Some(PurgaValue::StrVec(vec)) => {
+                // If the key exists and it's already a vector of strings, just push the new value
+                vec.push(value);
+            }
+            None => {
+                // If the key does not exist, insert it as a single String
+                map.insert(key, PurgaValue::Str(value));
+            }
+        }
+    }
+
+    map
+}
+
 fn main() {
     let cli_args = Args::parse();
-    let args: HashMap<String, String> = cli_args.args.into_iter().collect();
+    let args: HashMap<String, PurgaValue> = transform(cli_args.args);
+
     let json_args = json!(args);
 
     let command = format!(
